0b5a09428ef19489d6342e5b1109478e50cfbb45
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2024-09-08 13:31:42 +0200

    Setup code

---
+++ src/distances.py
@@ -0,0 +1,23 @@
+
+def load_data():
+    print("load data")
+
+def get_location():
+    print("retrieve coordinate")
+
+def calculate_distance():
+    print("calculate distance")
+
+
+def main():
+    # Load data
+    load_data()
+
+    # Calculate all coordinates
+    get_location()
+
+    # Calculate distance
+    calculate_distance()
+
+if __name__ == "__main__":
+    main()
